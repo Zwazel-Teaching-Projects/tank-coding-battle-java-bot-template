97d6ddc644c80386389d73f87795b3008c5ca793
Tim Irmler <zwazel@hotmail.com>
2025-02-02 18:32:10 +0100

    no comments

--- src/main/java/MyBot.java
+++ src/main/java/MyBot.java
@@ -4,9 +4,6 @@ import dev.zwazel.internal.PublicGameWorld;
 
 public class MyBot implements BotInterface {
     public static void main(String[] args) {
-        // This is the entry point of your bot
-        // You can use this method to initialize your bot
-        // and start the game loop
         GameWorld.startGame(new MyBot());
     }
 
