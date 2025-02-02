459de4da0fec35d3498c896d61562b412b7871de
Tim Irmler <zwazel@hotmail.com>
2025-02-02 16:56:31 +0100

    IT WORKS!

---
+++ src/main/java/MyBot.java
@@ -0,0 +1,13 @@
+import dev.zwazel.bot.BotInterface;
+import dev.zwazel.internal.PublicGameWorld;
+
+public class MyBot implements BotInterface {
+    public static void main(String[] args) {
+
+    }
+
+    @Override
+    public void processTick(PublicGameWorld publicGameWorld) {
+
+    }
+}
