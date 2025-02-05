5dd2c858484befd9c7f475cb2edf4dbde59d7992
Tim Irmler <zwazel@hotmail.com>
2025-02-05 18:53:25 +0100

    obviously it doesnt work, i'm using singletons -_-
    i need to think of something for testing purposes

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,9 +1,6 @@
 public class Main {
     public static void main(String[] args) {
-        Thread thread1 = new Thread(new MyBot());
-        Thread thread2 = new Thread(new MyBot());
-
-        thread1.start();
-        thread2.start();
+        MyBot bot = new MyBot();
+        bot.start();
     }
 }
--- src/main/java/MyBot.java
+++ src/main/java/MyBot.java
@@ -2,7 +2,7 @@ import dev.zwazel.GameWorld;
 import dev.zwazel.bot.BotInterface;
 import dev.zwazel.internal.PublicGameWorld;
 
-public class MyBot implements BotInterface, Runnable {
+public class MyBot implements BotInterface {
     public void start() {
         GameWorld.startGame(this);
     }
@@ -10,10 +10,5 @@ public class MyBot implements BotInterface, Runnable {
     @Override
     public void processTick(PublicGameWorld publicGameWorld) {
         System.out.println("Hello, world!");
-    }
-
-    @Override
-    public void run() {
-        start();
     }
 }
