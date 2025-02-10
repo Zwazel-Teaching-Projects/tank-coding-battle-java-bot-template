4a79e51138602113ced1a06743164799f1b19d72
Tim Irmler <zwazel@hotmail.com>
2025-02-10 10:43:24 +0100

    invalid target but everything got renamed

--- src/main/java/MyBot.java
+++ src/main/java/MyBot.java
@@ -1,10 +1,10 @@
 import dev.zwazel.GameWorld;
 import dev.zwazel.bot.BotInterface;
 import dev.zwazel.internal.PublicGameWorld;
-import dev.zwazel.internal.messages.MessageContainer;
-import dev.zwazel.internal.messages.data.SimpleTextMessage;
+import dev.zwazel.internal.message.MessageContainer;
+import dev.zwazel.internal.message.data.SimpleTextMessage;
 
-import static dev.zwazel.internal.messages.MessageTarget.Type.TEAM;
+import static dev.zwazel.internal.message.MessageTarget.Type.SERVER_ONLY;
 
 public class MyBot implements BotInterface {
     public void start() {
@@ -15,6 +15,6 @@ public class MyBot implements BotInterface {
     public void processTick(PublicGameWorld publicGameWorld) {
         System.out.println("Hello, world! " + publicGameWorld.getGameState().tick());
 
-        publicGameWorld.send(new MessageContainer(TEAM.get(), new SimpleTextMessage("Hello from MyBot!")));
+        publicGameWorld.send(new MessageContainer(SERVER_ONLY.get(), new SimpleTextMessage("Hello from MyBot!")));
     }
 }
