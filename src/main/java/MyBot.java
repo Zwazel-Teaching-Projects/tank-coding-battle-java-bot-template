8905931361ec539402caae1d9d02f2dc51acb564
Tim Irmler <zwazel@hotmail.com>
2025-02-28 16:17:43 +0100

    receiving information that we got hit!

--- src/main/java/MyBot.java
+++ src/main/java/MyBot.java
@@ -9,8 +9,11 @@ import dev.zwazel.internal.game.tank.Tank;
 import dev.zwazel.internal.game.tank.TankConfig;
 import dev.zwazel.internal.game.tank.implemented.LightTank;
 import dev.zwazel.internal.message.MessageContainer;
+import dev.zwazel.internal.message.MessageData;
 import dev.zwazel.internal.message.data.GameConfig;
 import dev.zwazel.internal.message.data.SimpleTextMessage;
+import dev.zwazel.internal.message.data.tank.GotHit;
+import dev.zwazel.internal.message.data.tank.Hit;
 
 import java.util.List;
 import java.util.Optional;
@@ -96,7 +99,31 @@ public class MyBot implements BotInterface {
 
         List<MessageContainer> messages = world.getIncomingMessages();
         for (MessageContainer message : messages) {
-            System.out.println("Received message:\n\t" + message);
+            MessageData data = message.getMessage();
+
+            switch (data) {
+                case SimpleTextMessage textMessage ->
+                        System.out.println("Received text message:\n\t" + textMessage.message());
+                case GotHit gotHitMessageData -> {
+                    ConnectedClientConfig shooterConfig = world.getConnectedClientConfig(gotHitMessageData.shooterEntity()).orElseThrow();
+                    System.out.println("Got hit by " + shooterConfig.clientName() + " on " + gotHitMessageData.hitSide());
+                    System.out.println("Received " + gotHitMessageData.damageReceived() + " damage!");
+                    System.out.println("Current health: " + myClientState.currentHealth());
+                    
+                    if (world.getMyState().state() == ClientState.PlayerState.DEAD) {
+                        System.out.println("I'm dead!");
+                    }
+                }
+                case Hit hitMessageData -> {
+                    ConnectedClientConfig targetConfig = world.getConnectedClientConfig(hitMessageData.hitEntity()).orElseThrow();
+                    ClientState targetState = world.getClientState(hitMessageData.hitEntity());
+                    System.out.println("Hit " + targetConfig.clientName() + " on " + hitMessageData.hitSide());
+                    System.out.println("Dealt " + hitMessageData.damageDealt() + " damage!");
+                    System.out.println(targetConfig.clientName() + " health: " + targetState.currentHealth());
+                }
+                default -> System.err.println("Received unknown message type: " + data.getClass().getSimpleName());
+            }
+
         }
 
         // Sending a nice message to all team members (individually, you could also send a single message to full team)
